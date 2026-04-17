7d603537fb860bc68546e73c5f059e7d384490f4
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-04-17 10:35:51 -0300

    feat: add index route

--- app.js
+++ app.js
@@ -11,12 +11,14 @@ const APP_PORT = process.env.APP_PORT
 const APP_HOST = process.env.APP_HOST
 
 const app = express()
+const indexRouter = require("./src/routes/index-route")
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use(cors())
+app.use("/", indexRouter)
 
 app.listen(APP_PORT, () => {
     console.log(`
