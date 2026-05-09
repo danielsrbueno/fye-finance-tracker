ed57f47c10594f3f81ab55e4c5e7ca912f9f2a0c
Daniel Bueno <111004405+danielsrbueno@users.noreply.github.com>
2026-05-08 21:50:58 -0300

    fix: category routes

--- src/routes/category-route.js
+++ src/routes/category-route.js
@@ -3,19 +3,19 @@ const router = express.Router()
 
 const categoryController = require("../controllers/category-controller")
 
-router.post("/", (req, res) => {
+router.post("/create", (req, res) => {
   categoryController.create(req, res)
 })
 
-router.put("/", (req, res) => {
+router.put("/update", (req, res) => {
   categoryController.update(req, res)
 })
 
-router.delete("/", (req, res) => {
+router.delete("/remove", (req, res) => {
   categoryController.remove(req, res)
 })
 
-router.get("/", (req, res) => {
+router.get("/all", (req, res) => {
   categoryController.getAllByUser(req, res)
 })
 
